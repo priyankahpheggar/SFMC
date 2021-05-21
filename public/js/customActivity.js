45aa42c86519c0440db756d5d9d7fb577a516dab
priyankahpheggar <69188044+priyankahpheggar@users.noreply.github.com>
2021-05-21 16:51:47 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -66,12 +66,12 @@ define([
     }
 
     function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
+        /*var postcardURLValue = $('#postcard-url').val();
+        var postcardTextValue = $('#postcard-text').val();*/
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+             "emailAddress": "{{InteractionDefaults.Email}}"
         }];
         
         payload['metaData'].isConfigured = true;
